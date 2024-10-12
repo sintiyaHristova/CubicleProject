9a75cf6e1bca22939a57b75924c13f34f628c2c8
Sintia Hristova <hristova.sintiya@gmail.com>
2024-10-12 18:34:57 +0300

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const routes = require('./routes');
+
 const config = require('./config'); 
 const setupViewEngine = require('./config/viewEngine');
 
@@ -7,9 +9,8 @@ const app = express();
 setupViewEngine(app);
 
 app.use(express.static('src/public'))
+app.use(routes);
 
-app.get('/', (req, res) => {
-    res.render('home')
-});
+
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
